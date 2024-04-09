dab907e76a4a401f0a047db74fe336b0c3479a5c
NumaDev <at.psicotrisquel@gmail.com>
2024-04-09 12:15:10 -0300

    Menu mobile finalizado. Chequear tamaño mediaQuery

---
+++ script.js
@@ -0,0 +1,13 @@
+// Abre y cierra el menu aplicando clases, y cambia el icono 
+function openClose() {
+    const menu = document.getElementById("menu");
+    const burger = document.querySelector('.menu-icon');
+    if (menu.className === "ul-nav") {
+        menu.className = "active";
+        burger.src = './img/minus-solid.svg'
+    } else {
+        menu.className = "ul-nav"
+        menu.style.display = "flex"
+        burger.src = './img/menu-hamburger.svg'
+    }
+}
