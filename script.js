fc55c3996741aecbf0d93c6a6d6a7501b88dd716
NumaDev <at.psicotrisquel@gmail.com>
2024-04-11 18:44:28 -0300

    Agrego color al header

--- script.js
+++ script.js
@@ -2,15 +2,12 @@
 function openClose() {
     const menu = document.getElementById("menu");
     const burger = document.querySelector('.menu-icon');
-    const nav = document.querySelector('nav');
     if (menu.className === "ul-nav") {
         menu.className = "active";
         burger.src = './img/minus-solid.svg'
-        // nav.style.backgroundColor = "tranparent"
     } else {
         menu.className = "ul-nav"
         menu.style.display = "block"
         burger.src = './img/menu-hamburger.svg'
-        // nav.style.backgroundColor = "tranparent"
     }
 }
