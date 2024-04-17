69f62fc011977102c3cfea27c2e78b36007779f2
NumaDev <at.psicotrisquel@gmail.com>
2024-04-17 12:53:55 -0300

    Modifica el nav, iconos, tipografia, colores. Agraga carpeta tipografias

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 // Abre y cierra el menu aplicando clases, y cambia el icono.
 function openClose() {
-    const menu = document.getElementById("menu");
-    const burger = document.querySelector('.menu-icon');
-    const nav = document.querySelector('nav');
+    const menu = document.getElementById("menu")
+    const burger = document.querySelector('.menu-icon')
+    const nav = document.querySelector('nav')
     //Open
-    if (menu.className === "ul-nav") {
-        menu.className = "active"; //Agrego clase .active a menu
-        burger.src = './img/minus-solid.svg' //Cmabio el icono
-        nav.style.display = "block";
+    if (nav.style.display === "none") {
+        menu.className = "active" //Agrego clase .active a menu
+        burger.src = './img/menu-open.svg' //Cmabio el icono
+        nav.style.display = "block"
     } //Close
     else {
         menu.className = "ul-nav"
         menu.style.display = "block"
-        burger.src = './img/menu-hamburger.svg'
+        burger.src = './img/menu-close.svg'
         nav.style.display = "none"
     }
 }
