5f5aaa690b5bfdfe56841570f27eaf1526b9e6d5
NumaDev <at.psicotrisquel@gmail.com>
2024-05-03 13:27:36 -0300

    Corrige error de referencia del icono del menu mobile

--- js/script.js
+++ js/script.js
@@ -1,20 +1,9 @@
-// Abre y cierra el menu aplicando clases, y cambia el icono.
-
-//Obtengo la url de donde es llamado el script
-let urlActual = window.location.href;
-
-//array con los nombres de los html que estan en la raiz
-let raiz = ["index", "carrito", "contacto", "sobre-nosotros"]
-
-//Busca si la url contiene alguna de las parabras del array
-let ifRaiz = raiz.some(e => urlActual.includes(e));
-
-
+// FALTA COPIAR EL NUEVO HEADER EN TODOS LOS ARCHIVOS
 function openClose() {
     // Obtengo el elemento con id = menu
     const menu = document.getElementById("menu")
     //obtengo el elemento con clase .menu-icon
-    const burger = document.querySelector('.menu-icon')
+    const burger = document.querySelector('#burger-icon')
     // obtengo la etiqueta nav
     const nav = document.querySelector('.nav-menu')
 
@@ -22,28 +11,17 @@ function openClose() {
     if (nav.style.display === "none") {
         // Agrego clase .active a menu
         menu.className = "active"
-        if (ifRaiz) {
-            // Cambio el icono abierto
-            burger.src = './img/menu-open.svg'
-        } else {
-            // Cambio el icono abierto
-            burger.src = '../img/menu-open.svg'
-        }
+        burger.classList.remove("close")
+        burger.classList.add("open")
         // Cambio el display del nav para que se vea
         nav.style.display = "block"
     } //Close
     else {
         // vuelvo a darle la clase anterior
         menu.className = "ul-nav"
-        if (ifRaiz) {
-            // Cambio el icono cerrado
-            burger.src = './img/menu-close.svg'
-        } else {
-            // Cambio el icono cerrado
-            burger.src = '../img/menu-close.svg'
-        }
+        burger.classList.remove("open")
+        burger.classList.add("close");
         // Vuelvo a ocultar el nav
         nav.style.display = "none"
     }
 }
-
