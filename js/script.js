4451e7bb8d4ad534d96567f0a576ed2dc4d17d34
NumaDev <at.psicotrisquel@gmail.com>
2024-05-07 13:20:30 -0300

    FIN DEL HEADER

--- js/script.js
+++ js/script.js
@@ -1,27 +1,23 @@
-// FALTA COPIAR EL NUEVO HEADER EN TODOS LOS ARCHIVOS
+
+// Obtengo el elemento con id menu
+const menu = document.getElementById("menu")
+//obtengo el elemento con clase .menu-icon
+const burger = document.querySelector('#burger-icon')
+// obtengo la etiqueta nav
+const nav = document.querySelector('.nav-menu')
 function openClose() {
-    // Obtengo el elemento con id = menu
-    const menu = document.getElementById("menu")
-    //obtengo el elemento con clase .menu-icon
-    const burger = document.querySelector('#burger-icon')
-    // obtengo la etiqueta nav
-    const nav = document.querySelector('.nav-menu')
 
     //Open
-    if (nav.style.display === "none") {
+    if (menu.className === "ul-nav") {
         // Agrego clase .active a menu
         menu.className = "active"
         burger.classList.remove("close")
         burger.classList.add("open")
-        // Cambio el display del nav para que se vea
-        nav.style.display = "block"
     } //Close
     else {
         // vuelvo a darle la clase anterior
         menu.className = "ul-nav"
         burger.classList.remove("open")
         burger.classList.add("close");
-        // Vuelvo a ocultar el nav
-        nav.style.display = "none"
     }
 }
