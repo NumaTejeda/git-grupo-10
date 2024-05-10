5c74e34ca5b325f892d64687c755b5bd1c4b2413
NumaDev <at.psicotrisquel@gmail.com>
2024-05-09 21:36:44 -0300

    Finaliza validacion de input-file.

--- js/form.js
+++ js/form.js
@@ -1,3 +1,40 @@
-/* Validacion de formulario */
+/*input-file*/
 
-const inputs = document.querySelectorAll(".")
+let inputFile = document.querySelector('#file');
+let labelFile = document.querySelector('#label-file');
+
+inputFile.addEventListener('change', () => {
+    if (inputFile.files.length > 0) {
+        let sizeFileMegas = inputFile.files[0].size / (1024 * 1024)
+        if (sizeFileMegas < 3) {
+            let nameArchivo = inputFile.files[0].name.slice(0, 10) + "...";
+            labelFile.textContent = nameArchivo;
+            labelFile.classList.add('checked-file');
+            labelFile.classList.remove('error-file');
+            console.log(inputFile.files[0])
+        } else {
+            console.error("Error: la imagen debe pesar menos que 3MB")
+            labelFile.textContent = "El archivo debe pesar menos de 3MB"
+            labelFile.classList.add('error-file');
+            labelFile.classList.remove("checked-file")
+        }
+    }
+})
+/*input mail*/
+
+let inputMail = document.querySelector("#mail")
+
+inputMail.addEventListener('input', () => {
+    let mail = inputMail.value.trim();
+    console.log(mail.length)
+    let regex = /\.\w{2,}$/;
+    if ((mail.length > 3) && (mail.indexOf('@') !== -1) && regex.test(mail)) {
+        console.log("correcto")
+
+
+    } else {
+        console.log("incorrect")
+
+
+    }
+})
--- js/script.js
+++ js/script.js
@@ -1,23 +1,15 @@
-
 // Obtengo el elemento con id menu
 const menu = document.getElementById("menu")
 //obtengo el elemento con clase .menu-icon
 const burger = document.querySelector('#burger-icon')
-// obtengo la etiqueta nav
-const nav = document.querySelector('.nav-menu')
-function openClose() {
 
-    //Open
+function openClose() {
+    // Open
     if (menu.className === "ul-nav") {
-        // Agrego clase .active a menu
         menu.className = "active"
-        burger.classList.remove("close")
-        burger.classList.add("open")
     } //Close
     else {
-        // vuelvo a darle la clase anterior
         menu.className = "ul-nav"
-        burger.classList.remove("open")
-        burger.classList.add("close");
     }
+    burger.classList.toggle("open")
 }
